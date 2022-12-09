9ecaa0ca91d37ab5b097f0532a2eeb44817bf067
Chandra Shekar Reddy <csreddy1998@gmail.com>
2022-12-09 00:49:57 -0600

    removed unused code

--- step-decay/step-lr-decay.py
+++ step-decay/step-lr-decay.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class StepLRDecay:
-    def __init__(self, optimizer, init_lr, decay_rate, decay_step):
-        self.optimizer = optimizer
+    def __init__(self, init_lr, decay_rate, decay_step):
         self.init_lr = init_lr
         self.decay_rate = decay_rate
         self.decay_step = decay_step
@@ -28,6 +27,5 @@ if __name__ == "__main__":
     init_lr = 0.1
     decay_rate = 0.9
     decay_step = 10
-    optimizer = None
-    step_lr_decay = StepLRDecay(optimizer, init_lr, decay_rate, decay_step)
+    step_lr_decay = StepLRDecay(init_lr, decay_rate, decay_step)
     step_lr_decay.plot(epochs)
